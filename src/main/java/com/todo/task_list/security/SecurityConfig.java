4e72cd4910b89e1c8ec8e275dbe4bcb36e857197
Thanyani266 <carlos@DESKTOP-0RO5JCM.localdomain>
2025-10-27 05:47:30 +0200

    Added User details service and Updated security config and application properties

--- src/main/java/com/todo/task_list/security/SecurityConfig.java
+++ src/main/java/com/todo/task_list/security/SecurityConfig.java
@@ -1,25 +1,28 @@
 package com.todo.task_list.security;
 
 import com.todo.task_list.repository.BlacklistedTokenRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.config.Customizer;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     private final JwtUtil jwtUtil;
     private final BlacklistedTokenRepository blacklistedTokenRepository;
-
-    public SecurityConfig(JwtUtil jwtUtil,
-                          BlacklistedTokenRepository blacklistedTokenRepository) {
-        this.jwtUtil = jwtUtil;
-        this.blacklistedTokenRepository = blacklistedTokenRepository;
-    }
+    private final UserDetailsService userDetailsService;
 
     @Bean
     public JwtAuthenticationFilter jwtAuthenticationFilter() {
@@ -32,15 +35,31 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/", "/auth/**").permitAll()  // login/register public
-                .requestMatchers("/tasks/**").hasRole("USER") // tasks require ROLE_USER
+                .requestMatchers("/", "/auth/**").permitAll()
+                .requestMatchers("/tasks/**").hasRole("USER")
                 .anyRequest().authenticated()
             )
-            .httpBasic(httpBasic -> httpBasic.disable()) // disable HTTP Basic authentication
-            .formLogin(form -> form.disable())
+            .authenticationProvider(authenticationProvider())
             .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-    
+
         return http.build();
     }
 
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
 }
