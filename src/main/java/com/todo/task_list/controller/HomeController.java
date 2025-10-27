ab207108ee1ba73227f699d797a48e2bff4677c8
Thanyani266 <carlos@DESKTOP-0RO5JCM.localdomain>
2025-10-27 21:06:03 +0200

    Added Home Controller

---
+++ src/main/java/com/todo/task_list/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.todo.task_list.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "TaskList app is running!";
+    }
+}
